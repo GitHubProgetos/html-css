275b05b7e5283f9153735dbfb2290c4f77959f12
GitHubProgetos/html-css <reginaldomachadodasilva16@gmail.com>
2023-02-13 19:26:46 -0300

    at

--- exercicios/god-of-war-ragnarok/js/main.js
+++ exercicios/god-of-war-ragnarok/js/main.js
@@ -1,4 +1,13 @@
+var swiper = new Swiper(".slide-characters", {
+    slidesPerView: 3.5,
+    spaceBetween: 19,
+    freeMode:true,
+   
+  });
+
 AOS.init({
     duration:1000,
     once:true,
 });
+
+
